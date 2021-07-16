4cc891f92fbe9f32f39cf499745300e0ac7e5301
danitrod <danitrod@outlook.com>
2021-07-16 02:22:46 -0300

    add response

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::io::prelude::*;
-use std::net::TcpListener;
-use std::net::TcpStream;
+use std::{
+    fs,
+    io::prelude::*,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,20 +16,29 @@ fn main() {
 
 // We need mut for stream, even though we are just gonna read it, because reading
 // a stream can mutate its internal value. If more data is to be read than what
-// can be allocated to the buffer, the possible data will be read, and the
+// can be allocated to the buffer, the fitting data will be read, and the
 // remaining data will keep in the variable.
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 4];
+    let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
 
-    println!("First 4 bytes: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = fs::read_to_string("hello.html").unwrap();
 
-    let mut remaining_buffer = [0; 1024];
-    stream.read(&mut remaining_buffer).unwrap();
-
-    println!(
-        "Remaining bytes: {}",
-        String::from_utf8_lossy(&remaining_buffer[..])
+    /*  The HTTP response should look something like this:
+    HTTP-Version Status-Code Reason-Phrase CRLF
+    headers CRLF
+    CRLF
+    body
+    */
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
     );
+
+    // The same stream variable is also used to write the response
+    stream.write(response.as_bytes()).unwrap();
+    // Flush asures the writing is successfully completed before dropping the connection
+    stream.flush().unwrap();
 }
